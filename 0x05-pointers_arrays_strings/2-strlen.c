094085d8f5213db10defc80081b7b1b847c18709
Almekdad-ZEMZGUI <mekzemzgui@gmail.com>
2023-06-26 08:00:02 -0700

    third task

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * _strlen - returns the length of a string
+ * @s: string
+ * Return: length
+ */
+int _strlen(char *s)
+{
+	int l = 0;
+	while (s[l] != '\0'){
+	l++;
+}
+	return l;
+}
