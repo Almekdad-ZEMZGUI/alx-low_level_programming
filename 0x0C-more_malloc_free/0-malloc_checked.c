f3ead5dfe17bd3030e16ccfddde34c57507da40c
Almekdad-ZEMZGUI <mekzemzgui@gmail.com>
2024-01-18 07:33:01 -0800

    first commit

---
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,19 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include "math.h"
+/**
+ * create_pointer - pointer to allocates memory
+ * @b: int
+ * Description: create a pointer
+ * Return: pointer to the allocated memory
+ *
+ */
+void *malloc_checked(unsigned int b){
+	int *ptr;
+
+	ptr = malloc(sizeof(int) * b);
+	if(ptr == NULL){
+		exit(98);
+	}
+	return (ptr);
+}
