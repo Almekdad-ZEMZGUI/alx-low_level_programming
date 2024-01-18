f225ab8eb689ca820b045c19655d1ca6d4935f93
Almekdad-ZEMZGUI <mekzemzgui@gmail.com>
2024-01-18 08:17:30 -0800

    sec

--- 0x0C-more_malloc_free/0-malloc_checked.c
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -4,16 +4,17 @@
 /**
  * create_pointer - pointer to allocates memory
  * @b: int
- * Description: create a pointer
+ * Description: allocate memory
  * Return: pointer to the allocated memory
  *
  */
-void *malloc_checked(unsigned int b){
+void *malloc_checked(unsigned int b)
+{
 	int *ptr;
 
 	ptr = malloc(sizeof(int) * b);
-	if(ptr == NULL){
+	if (ptr == NULL)
 		exit(98);
-	}
+
 	return (ptr);
 }
