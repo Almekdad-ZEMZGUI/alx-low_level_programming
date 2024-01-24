f2306ef047f7830d26d203a08878a1681c7bb6d8
Almekdad-ZEMZGUI <mekzemzgui@gmail.com>
2024-01-24 06:50:31 -0800

    second task

--- 0x0F-function_pointers/function_pointers.h
+++ 0x0F-function_pointers/function_pointers.h
@@ -1,6 +1,8 @@
 #ifndef _FUNC_POINTERS_H_
 #define _FUNC_POINTERS_H_
+#include <stddef.h>
 
 void print_name(char *name, void (*f)(char *));
+void array_iterator(int *array, size_t size, void (*action)(int));
 
 #endif /* _FUNC_POINTERS_H_*/
