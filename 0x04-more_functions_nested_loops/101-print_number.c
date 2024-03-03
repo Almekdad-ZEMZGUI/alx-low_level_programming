44acb8e632d4c37fcd29619af950e72386d19c21
Almekdad-ZEMZGUI <mekzemzgui@gmail.com>
2024-03-03 13:13:53 -0800

    13th task

---
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -0,0 +1,35 @@
+#include "main.h"
+
+/**
+ * print_number - prints an integer
+ * @n: the integer to print
+ *
+ * Return: nothing
+ */
+void print_number(int n)
+{
+	int temp, z = 1, num = n;
+
+	if (n == 0)
+		_putchar('0');
+	else
+	{
+		if (n < 0)
+		{
+			_putchar('-');
+			num = -n;
+		}
+		while (z <= num)
+			z *= 10;
+		z /= 10;
+
+		while (z >= 1)
+		{
+			temp = num / z;
+			_putchar(temp + 48);
+			num = (num - (z * temp));
+			z /= 10;
+		}
+	}
+}
+
