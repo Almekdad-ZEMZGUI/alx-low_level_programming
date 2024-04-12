d9f6dc792dc50e535694fdd5af5489c8ae4eb5e8
Almekdad-ZEMZGUI <mekzemzgui@gmail.com>
2024-04-12 07:08:52 -0700

    task 0

---
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -0,0 +1,26 @@
+#include "hash_tables.h"
+
+/**
+ * hash_table_create - Creates a hash table
+ * @size: the size of the array
+ *
+ * Return: pointer to the newly created hash table
+*/
+hash_table_t *hash_table_create(unsigned long int size)
+{
+        hash_table_t *new_table;
+        unsigned long int i;
+
+        new_table = malloc(sizeof(hash_table_t));
+        if (!new_table)
+                return (NULL);
+        new_table->array = malloc(sizeof(hash_node_t) * size);
+        if (!new_table->array)
+                return (NULL);
+        
+        for (i = 0; i < size; i++)
+                array[i] = NULL;
+        
+        new_table->size = size;
+        return (new_table)
+} 
